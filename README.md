169be0f2629f1a816a49bc62a9cc2fe5d0dd5bf5
Tomas Korenko <radixxko@gmail.com>
2018-10-03 13:46:33 +0200

    Version 1.0.0

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # liqd-options
+
+[![NPM version](https://img.shields.io/npm/v/liqd-options.svg)](https://www.npmjs.com/package/liqd-options)
+[![Build Status](https://travis-ci.org/radixxko/liqd-options.svg?branch=master)](https://travis-ci.org/radixxko/liqd-options)
+[![Coverage Status](https://coveralls.io/repos/github/radixxko/liqd-options/badge.svg?branch=master)](https://coveralls.io/github/radixxko/liqd-options?branch=master)
+[![NPM downloads](https://img.shields.io/npm/dm/liqd-options.svg)](https://www.npmjs.com/package/liqd-options)
+[![MIT License](https://img.shields.io/badge/license-MIT-blue.svg)](LICENSE)
---
+++ test/test.js
@@ -0,0 +1,182 @@
+const assert = require('assert' );
+
+const Options = require('../lib/options');
+
+describe( 'Tests', () =>
+{
+	it('should validate options', function()
+	{
+		assert.deepStrictEqual
+		(
+			Options(
+			{
+				foo: 'bar',
+				bar: 'foo',
+				tests:
+				{
+					foo: 'foo',
+					bar : 42,
+					foobar: '256',
+					barfoo: 11,
+					over: true,
+				},
+				pass:
+				{
+					foo: 'bar',
+					bar: false
+				},
+				missing:
+				{
+
+				},
+				def: 42
+			},
+			{
+				foo: { _required: false },
+				def: 'foobar',
+				fed: { _default: 'barfoo' },
+				tests:
+				{
+					_required: true,
+					foo: { _any: ['foo', 'bar'], _requires: '.bar' },
+					bar: { _type: [ 'string', 'number' ] },
+					foobar: { _type: 'string', _convert: parseInt },
+					barfoo: { _passes: $ => 10 < $ && $ < 12 }
+				},
+				pass: { _required: true },
+				missing:
+				{
+					foo: true,
+					bar: { _default: 'foo', _requires: ['..tests.foo', 'foo'] },
+					foobar: { _default: { foo: 'bar' } },
+					barfoo: { _requires: 'foo' }
+				},
+				def: 64,
+				fed: 48
+			}),
+			{
+				foo: 'bar',
+				def: 'foobar',
+				fed: 'barfoo',
+				tests:
+				{
+					foo: 'foo',
+					bar : 42,
+					foobar: 256,
+					barfoo: 11
+				},
+				pass:
+				{
+					foo: 'bar',
+					bar: false
+				},
+				missing:
+				{
+					foo: true,
+					bar: 'foo',
+					foobar:
+					{
+						foo: 'bar'
+					}
+				},
+				def: 42,
+				fed: 48
+			}
+		);
+	});
+
+	it('should fail - not object', function()
+	{
+		try
+		{
+			Options({ foo: null }, { foo: { bar: { _required: true }}});
+
+			assert.fail();
+		}
+		catch(e){ assert.equal( e.message, 'Invalid options: "foo" is of wrong type null, "object" is required' ); }
+	});
+
+	it('should fail - required', function()
+	{
+		try
+		{
+			Options({ foo: {}}, { foo: { bar: { _required: true }}});
+
+			assert.fail();
+		}
+		catch(e){ assert.equal( e.message, 'Invalid options: "foo.bar" is required' ); }
+	});
+
+	it('should fail - type missmatch', function()
+	{
+		try
+		{
+			Options({ foo: { bar: 'test' }}, { foo: { bar: { _type: 'number' }}});
+
+			assert.fail();
+		}
+		catch(e){ assert.equal( e.message, 'Invalid options: "foo.bar" is of wrong type "string", "number" is required' ); }
+	});
+
+	it('should fail - value missmatch', function()
+	{
+		try
+		{
+			Options({ foo: { bar: 'test' }}, { foo: { bar: { _any: ['foo', 'bar'] }}});
+
+			assert.fail();
+		}
+		catch(e){ assert.equal( e.message, 'Invalid options: "foo.bar" is of wrong value "test", "foo" or "bar" is required' ); }
+	});
+
+	it('should fail - value does not pass', function()
+	{
+		try
+		{
+			Options({ foo: { bar: 'test' }}, { foo: { bar: { _passes: $ => $ === 'foo' }}});
+
+			assert.fail();
+		}
+		catch(e){ assert.equal( e.message, 'Invalid options: "foo.bar" is of wrong value "test", ( $ => $ === \'foo\' ) is required' ); }
+	});
+
+	it('should fail - requirements not met', function()
+	{
+		try
+		{
+			Options({ foo: { bar: { fooobar: { barfoo: true } } }}, { foo: { bar: { fooobar: { barfoo: { _requires: '.val' }}}}});
+
+			assert.fail();
+		}
+		catch(e){ assert.equal( e.message, 'Invalid options: "foo.bar.fooobar.val" does not meet the requirements of "foo.bar.fooobar.barfoo"' ); }
+
+		try
+		{
+			Options({ foo: { bar: { fooobar: { barfoo: true } } }}, { foo: { bar: { fooobar: { barfoo: { _requires: '..val' }}}}});
+
+			assert.fail();
+		}
+		catch(e){ assert.equal( e.message, 'Invalid options: "foo.bar.val" does not meet the requirements of "foo.bar.fooobar.barfoo"' ); }
+
+
+		try
+		{
+			Options({ foo: { bar: { fooobar: { barfoo: true } } }}, { foo: { bar: { fooobar: { barfoo: { _requires: 'bar' }}}}});
+
+			assert.fail();
+		}
+		catch(e){ assert.equal( e.message, 'Invalid options: "bar" does not meet the requirements of "foo.bar.fooobar.barfoo"' ); }
+	});
+});
+/*
+_required 	: true,
+_type 		: [ 'object', 'string', 'number' ],
+_any 		: [ '2', 54 ],
+_passes		: $ => 52 < $ && $ < 89,
+_convert	: $ => $ + 5,
+_default 	:
+{
+	a: 'b'
+},
+_requires 	: [ '.asdas', '.dasda' ]
+*/
